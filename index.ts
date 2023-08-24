286d4a09fc5f2245077105eab76ccde1b50d7c42
PinkiePie-tech <62440844+PinkiePie-tech@users.noreply.github.com>
2023-08-24 14:15:40 +0200

    Initial commit

---
+++ index.ts
@@ -0,0 +1,19 @@
+import './style.css';
+
+// Bienvenue dans ce tuto RxJs, nous allons voir aujourd'hui RxJs, étonnant non ?
+// Pour ce faire, pas de front, pas de html, du typescript comme on aime en voir
+// C'est là, la beauté de ce tuto, pas de chichi, que du bon gros code de dev back :D
+
+// La première étape est simple, créer un observable contenant une valeur et l'affiché dans un console.log().
+
+// La deuxième étape est moins simple, créer un timer et afficher "coucou" dans un console.log() à la fin de ce timer.
+
+// La troisième étape est encore moins simple, créer un interval et afficher "j'ai dit x fois "coucou"" dans un console.log(), x étant le nombre de fois que l'interval à été provoqué.
+
+// La quatrième étape est encore encore moins simple, créer un interval et afficher "j'ai dit x fois "coucou"" dans un console.log() à la fin de ce timer, x étant le nombre de fois que l'interval à été provoqué et arrêté de l'afficher à partir de 10 itérations.
+
+// La cinquième étape est encore encore encore moins simple, créer un timer qui au bout de 5s créer un interval et affiche "j'ai dit x fois "coucou"" dans un console.log(), x étant le nombre de fois que l'interval à été provoqué et arrêté de l'afficher à partir de 10 itérations.
+
+// La sixième étape est encore encore encore encore moins simple, créer un Observable contenant la valeur "5" et l'affiché au bout de 5s.
+
+// La septième étape est encore encore encore encore encore moins simple, créer un BehaviorSubject avec pour valeur par défaut 5, l'affiché, et toutes les 2s, ajouter 15 à cette valeur.
